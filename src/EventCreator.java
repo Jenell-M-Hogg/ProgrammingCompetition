031b1a63f0dfa23cec39ff16592abae6608c1666
ezzeldin <tahoune@mcmaster.ca>
2016-10-22 19:39:58 -0400

    rec
    rec first lines

--- src/EventCreator.java
+++ src/EventCreator.java
@@ -10,30 +10,62 @@ public class EventCreator {
 	//Tells boats what's going on
 	
 
+	Random rand = new Random();// randomness
 	
 	
-	int buoys= 3, boats = 2;
-	ArrayList<Buoy> Buoy_array;
-	ArrayList<Boat> Boat_array; 
+	int  buoys = 3;//rand.nextInt(10) + 1; // rand 1 to 10
+	int  boats = 1;//rand.nextInt(10) + 1; // rand 1 to 10
+	
+	
+	
+	ArrayList<Buoy> Buoy_array= new ArrayList<Buoy>();
+	ArrayList<Boat> Boat_array= new ArrayList<Boat>();
+	
 	
 	public EventCreator(){
-		generateEvent(); 
+		generateEvent(); // start a new simulation ( EVENT )  
 	}
 
+	
 	public void generateEvent() {
 		
 		// starts with initiating stuff
 
 		for ( int i = 0 ; i < buoys ; i++)
 			{ 
-			Buoy_array.add( new Buoy()); // create a bouy 
+
+			int  r_1_50_a = rand.nextInt(50) + 1; // rand 1 to 50
+			int  r_1_50_b = rand.nextInt(50) + 1; // rand 1 to 50
+			Point p1 = new Point(r_1_50_a,r_1_50_b) ; // rand point for position 
+		
+			Buoy_array.add( new Buoy(p1,String.valueOf(i)); // create a bouy 
+			}
+		
+		
+		for ( int i = 0 ; i < boats ; i++)
+			{ 
+
+			int  r_1_50_a = rand.nextInt(50) + 1; // rand 1 to 50
+			int  r_1_50_b = rand.nextInt(50) + 1; // rand 1 to 50
+			Point p1 = new Point(r_1_50_a,r_1_50_b) ; // rand point for position 
+			Point p2 = new Point(v_x,v_y) ; // point for position 
+			
+			int v_x=3  , v_y = 3; // boats' velocities 
+			
+			Boat_array.add( new Boat(p1,p2,String.valueOf(i))); // create a boat
 			
 			}
-		for ( int i = 0 ; i < boats ; i++)
-		{ 
-		Boat_array.add( new Boat()); // create a bouy 
 		
+		// call air 
+		
+		while ( 1==1)
+		{
+		
+			// inifinte loop 
+			// breaks with stop button from gui 
+			
 		}
+		
 	}
 	
 	
