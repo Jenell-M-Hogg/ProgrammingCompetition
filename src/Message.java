45cc9921e6e87f8a1b92d7b2a528cec9cd2c3d68
Jenell-M-Hogg <jenellmhogg@live.com [mergetool]>
2016-10-22 18:40:05 -0400

    Updating Message
    Added pass and generateMessage functions, fixed constructor

--- src/Message.java
+++ src/Message.java
@@ -2,17 +2,41 @@ import java.awt.Point;
 
 public class Message {
 	boolean isEmergency;
-	String message; 
-	Point sourcePosition; 
-	int range;
+	
+	String message; // A message for printing- conveys information
+	
+	String id; //A unique identifier for the identifier
+	
+	Point sourcePosition; //The source origin of the message
+	
+	int range; //A transmission range- how far the message can go
+	
+	//The original origin point 
+	int timesSent=0;
+	//Number of times the message has been sent. Should be reset
 	
 	public Message(boolean isEmergency, String message, Point sourcePosition, int range){
 		this.isEmergency = isEmergency;
-		this.message = message; 
+		this.message = this.generateMessage(message, sourcePosition);
 		this.sourcePosition = sourcePosition;
 		this.range = range;
 		
 	}
+	
+	public void pass(Point newSource){
+		this.sourcePosition = newSource;
+		this.timesSent = 0;
+		
+	}
+	
+	private String generateMessage(String event, Point source){
+		//TODO
+		return null;
+	}
+	
+	
+	
+	
 
 	public boolean isEmergency() {
 		return isEmergency;
@@ -46,6 +70,9 @@ public class Message {
 		this.range = range;
 	}
 	
+}
+	
+	
 	
 
-}
+
