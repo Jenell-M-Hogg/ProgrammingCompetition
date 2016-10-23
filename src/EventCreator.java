b9202d14123c950bea832e7bd020e86112fb1ad7
ezzeldin <tahoune@mcmaster.ca>
2016-10-22 20:59:38 -0400

    fin

--- src/EventCreator.java
+++ src/EventCreator.java
@@ -53,25 +53,33 @@ public class EventCreator {
 			}
 		
 		
-		while ( ev_c < bigrand )
+		while ( ev_c < bigrand ) // runs bigrand number of storms 
 		{ ev_c++ ; // events counter 
 		
 		Point p1 = new Point(rand.nextInt(50)+1,rand.nextInt(50)+1) ; // rand point for position 
 		Message strm = new Message(true, "storm", p1, 25); // storm in some random place 
 		
+		Message hurr = new Message(true, "hurricane", p1, 50); // storm in some random place 
 		
+		
+		
+		Message msg = rand.nextInt(2)==1 ? strm : hurr; // randomly selct strm or hurr   
 		
 		for ( int i = 0 ; i< air.boats.size(); i++)
-		{
 		
-		if (  5 > Math.sqrt( Math.pow( (p1.getX()-air.boats.get(i).position.getX()),2)+
+			if (  5 > Math.sqrt( Math.pow( (p1.getX()-air.boats.get(i).position.getX()),2)+
+		
 				                     Math.pow((p1.getY()-air.boats.get(i).position.getY()),2)))
-			air.boats.;
+			air.boats.get(i).messages[0]= msg; // boats in range get msg 
 		
+		for ( int i = 0 ; i< air.buoys.size(); i++)
 			
+			if (  5 > Math.sqrt( Math.pow( (p1.getX()-air.buoys.get(i).position.getX()),2)+
+					                     Math.pow((p1.getY()-air.buoys.get(i).position.getY()),2)))
+				air.buoys.get(i).messages[0]= msg; // buoys in rng get msg 
+				
 			
-			
-		} // inifinte loop 
+		 // inifinte loop 
 			// breaks with stop button from gui 
 		
 		
